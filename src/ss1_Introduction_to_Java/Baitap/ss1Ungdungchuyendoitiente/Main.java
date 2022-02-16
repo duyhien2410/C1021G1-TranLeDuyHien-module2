359d00c54e93690d19b9add31e8f3289cc7c5d39
duyhien2410 <hienduy2410@gmail.com>
2022-02-16 17:02:24 +0700

    up

--- src/ss1_Introduction_to_Java/Baitap/ss1Ungdungchuyendoitiente/Main.java
+++ src/ss1_Introduction_to_Java/Baitap/ss1Ungdungchuyendoitiente/Main.java
@@ -4,13 +4,11 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        double vnd=23500;
         double usd;
-        Scanner scanner=new Scanner(System.in);
-        System.out.println("Enter USD: ");
-        usd=scanner.nextDouble();
-        double convert=usd*23500;
-        System.out.println("convert VND = "+convert);
-        
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter USD:");
+        usd= sc.nextDouble();
+        double convert=usd*23000;
+        System.out.println("convert VND: "+convert);
     }
 }
--- src/ss1_Introduction_to_Java/Baitap/ss1Ungdungdocsothanhchu/Main.java
+++ src/ss1_Introduction_to_Java/Baitap/ss1Ungdungdocsothanhchu/Main.java
@@ -4,96 +4,45 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        while (1 > 0) {
-            System.out.print("nhập số cần đọc (0-999): ");
-            String input = sc.nextLine();
-            while (!(input.matches("^[1-9][0-9]?[0-9]$") ||
-                    input.matches("^[0-9]$"))) {
-                System.out.println("không hợp lệ!");
-                System.out.print("nhập số cần đọc (0-999): ");
-                input = sc.nextLine();
-            }
-            String[] inputRay = input.split("");
-            switch (inputRay.length - 1) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Nhập số cần đọc:");
+        int number = sc.nextInt();
+
+        if (number>0 && number<11){
+            switch (number){
                 case 1:
-                    if (inputRay[1].equals("0")) {
-                        System.out.println("zero");
-                    } else {
-                        System.out.println(tenDown(inputRay[1]));
-                    }
+                    System.out.println("một");
                     break;
                 case 2:
-                    System.out.println(tenUp(inputRay[1], tenDown(inputRay[2])));
+                    System.out.println("hai");
                     break;
                 case 3:
-                    if ((inputRay[2].equals("0")) && (inputRay[3].equals("0"))) {
-                        System.out.println(hunredUp(inputRay[1]));
-                    } else {
-                        System.out.print(hunredUp(inputRay[1]));
-                        System.out.print(" and "+tenUp(inputRay[2], tenDown(inputRay[3])) + "\n");
-                    }
+                    System.out.println("ba");
+                    break;
+                case 4:
+                    System.out.println("bốn");
+                    break;
+                case 5:
+                    System.out.println("năm");
+                    break;
+                case 6:
+                    System.out.println("sáu");
+                    break;
+                case 7:
+                    System.out.println("bảy");
+                    break;
+                case 8:
+                    System.out.println("tám");
+                    break;
+                case 9:
+                    System.out.println("chín");
+                    break;
+                case 10:
+                    System.out.println("mười");
+                    break;
+                default:
+                    System.out.println("vượt quá hệ thống");
             }
-
         }
-
-    }
-
-    private static String tenDown(String number) {
-        switch (number) {
-            case "0":
-                return "";
-            case "1":
-                return "one";
-            case "2":
-                return "two";
-            case "3":
-                return "three";
-            case "4":
-                return "four";
-            case "5":
-                return "five";
-            case "6":
-                return "six";
-            case "7":
-                return "seven";
-            case "8":
-                return "eight";
-            case "9":
-                return "nine";
-            default:
-                return "can't read this";
-        }
-    }
-
-    private static String tenUp(String number, String previous) {
-        switch (number) {
-            case "0":
-                return previous;
-            case "1":
-                switch (previous) {
-                    case "":
-                        return "ten";
-                    case "one":
-                        return "eleven";
-                    case "two":
-                        return "twelve";
-                    case "three":
-                        return "thirdteen";
-                    default:
-                        return previous + "teen";
-                }
-            case "2":
-                return "twenty " + previous;
-            case "3":
-                return "thirdty " + previous;
-            default:
-                return tenDown(number) + "ty" + " " + previous;
-
-        }
-    }
-
-    private static String hunredUp(String number) {
-        return tenDown(number) + " hunred";
     }
 }
