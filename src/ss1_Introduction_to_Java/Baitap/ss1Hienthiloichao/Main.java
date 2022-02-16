7623fa15948291061564340e824455e16451cb9e
duyhien2410 <hienduy2410@gmail.com>
2022-02-16 21:29:53 +0700

    ip

--- src/ss1_Introduction_to_Java/Baitap/ss1Hienthiloichao/Main.java
+++ src/ss1_Introduction_to_Java/Baitap/ss1Hienthiloichao/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Nhập tên của bạn:");
+        System.out.println("Nhập tên của bạn: ");
         String name = scanner.nextLine();
         System.out.println("Hello "+name);
     }
